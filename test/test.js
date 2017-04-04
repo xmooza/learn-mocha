e6af85d6f392127ba6793634e5d8423db75bf619
m.mirza93 <m.mirza93@live.com>
2017-04-04 19:15:20 -0400

    Fixing the build

--- test/test.js
+++ test/test.js
@@ -13,11 +13,11 @@ describe('Cash Register', function(){
     }); // use .deepEqual for arrays see: http://stackoverflow.com/questions/13225274/
 
     it('getChange(486,1000) should equal [500, 10, 2]', function(){
-        assert.deepEqual(C.getChange(486,1000), [500, 10, 2]);
+        assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
     });
 
     it('getChange(1487,10000) should equal [5000, 2000, 500, 10, 2, 1 ]', function(){
-        assert.deepEqual(C.getChange(1487,10000), [5000, 2000, 500, 10, 2, 1 ]);
+        assert.deepEqual(C.getChange(1487,10000), [5000, 2000, 1000, 500, 10, 2, 1 ]);
     });
 
     // try to force an error:
